994345a57ad9ece3be0b204ca250342d02de789c
IsCaS-hub <sunjing24@iscas.ac.cn>
2024-11-23 00:56:31 +0800

    Add files via upload

---
+++ GRU/model.py
@@ -0,0 +1,33 @@
+
+import torch
+import torch.nn as nn
+import config
+import torch
+from torch.nn import Module, LSTM, Linear, GRU, RNN
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
+import numpy as np
+
+from zmq import device
+import torch.nn.functional as F   
+import matplotlib.pyplot as plt
+import json
+
+class Net_Decoder_T(Module): 
+    def __init__(self, config):
+        super(Net_Decoder_T, self).__init__()
+        self.GRU = GRU(input_size=config.model_DT_config["input_size"], 
+                       hidden_size=config.model_DT_config["hidden_size"],
+                       num_layers=config.model_DT_config["num_layers"], 
+                       batch_first=True, 
+                       dropout=config.model_DT_config["dropout"], 
+                       bidirectional=False)
+        
+        self.linear1 = Linear(in_features=config.model_DT_config["hidden_size"], out_features=config.model_DT_config["hidden_size"])
+        self.linear2 = Linear(in_features=config.model_DT_config["hidden_size"], out_features=config.model_DT_config["output_size"])
+        
+    def forward(self, x, hidden=None):
+        gru_out, h_n = self.GRU(x, hidden)  
+        linear_out = torch.tanh(self.linear1(gru_out[:, -1, :]))
+        linear_out = self.linear2(linear_out)
+        return linear_out, h_n  
